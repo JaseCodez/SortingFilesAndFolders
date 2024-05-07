02ddc5b9ef39de599365d5593c7df8067226d36c
JaseCodez <135294048+JaseCodez@users.noreply.github.com>
2024-05-06 21:48:21 -0400

    Update FileOrganizer.py

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -1,38 +1,58 @@
+"""
+Authors: Jason Phan
+Updated: 6th May 24'
+
+Organize files and folders alphabetically
+"""
+
 import os
 import shutil
 
 
 class FileOrganizer:
-    def __init__(self, directory: str):
+
+    directory: str
+    map: dict[str, list[str]]
+
+    def __init__(self, directory: str) -> None:
         self.directory = directory
         self.map = {}
 
         for item in os.listdir(self.directory):
             self.map.setdefault(item[0].upper(), []).append(item)
 
-    def organize_files(self):
+    def organize_files(self) -> None:
         self._create_folder()
         for key, values in self.map.items():
             for value in values:
-                if '.' in value:
+                src = os.path.join(self.directory, value)
+                if not os.path.isdir(src):
                     dst = os.path.join(self.directory, key) + '\\'
-                    src = os.path.join(self.directory, value)
-                    shutil.move(src=src, dst=dst)
+                    try:
+                        shutil.move(src=src, dst=dst)
+                    except shutil.Error:
+                        print(f'{value} could not be moved')
 
-    def organize_folders(self):
+    def organize_folders(self) -> None:
         self._create_folder()
         self._create_folder()
         for key, values in self.map.items():
             for value in values:
-                if '.' not in value:
+                src = os.path.join(self.directory, value)
+                if os.path.isdir(src):
                     dst = os.path.join(self.directory, key) + '\\'
-                    src = os.path.join(self.directory, value)
-                    shutil.move(src=src, dst=dst)
+                    try:
+                        shutil.move(src=src, dst=dst)
+                    except shutil.Error:
+                        print(f'{value} could not be moved')
 
     def organize(self):
         self.organize_files(), self.organize_folders()
 
-    def _create_folder(self):
+    def organize_new_path(self, directory) -> None:
+        self.directory = directory
+
+    def _create_folder(self) -> None:
         for key in self.map.keys():
             path = os.path.join(self.directory, key)
             if not os.path.isdir(path):
